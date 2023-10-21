632754f8b9025483fe61b6148f55ebb39b7b0d6c
Victor Castro <victormatheus.7@hotmail.com>
2023-10-21 03:27:41 -0300

    Adding unit test cases.

---
+++ UnitTests/Application/ReservationServiceTests.cs
@@ -0,0 +1,83 @@
+﻿namespace UnitTests.Application
+{
+    internal class ReservationServiceTests
+    {
+        [Theory]
+        public void CreateReservation_WithValidInformation_ReturnsCreatedReservation()
+        {
+            Assert.Fail();
+        }
+
+        [Theory]
+        public void CreateReservation_WithInvalidInformation_ReturnsError()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GetReservations_AsCustomer_ReturnsCustomerReservations()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GetReservations_AsAdministrator_ReturnsAllReservations()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GetSpecificReservation_AsCustomerWithPermission_ReturnsReservation()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GetSpecificReservation_AsCustomerWithoutPermission_ReturnsError()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GetSpecificReservation_AsAdministrator_ReturnsReservation()
+        {
+            Assert.Fail();
+        }
+
+        [Theory]
+        public void UpdateReservation_WithPermission_ReturnsUpdatedReservation()
+        {
+            Assert.Fail();
+        }
+
+        [Theory]
+        public void UpdateReservation_WithoutPermission_ReturnsError()
+        {
+            Assert.Fail();
+        }
+
+        [Theory]
+        public void DeleteReservation_WithPermission_ReturnsSuccess()
+        {
+            Assert.Fail();
+        }
+
+        [Theory]
+        public void DeleteReservation_WithoutPermission_ReturnsError()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GetReservationsCountByDay_OfADayWithReservations_ReturnsReservationsCountByDay()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void GetReservationsCountByDay_OfADayWithoutReservations_ReturnsZero()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ UnitTests/Application/UserServiceTests.cs
@@ -0,0 +1,41 @@
+﻿namespace UnitTests.Application
+{
+    internal class UserServiceTests
+    {
+        [Test]
+        public void CreateUser_WithValidInformation_ReturnsSuccess()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CreateUser_WithInvalidEmail_ReturnsError()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CreateUser_WithInvalidPassword_ReturnsError()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CreateUser_WithAlreadyUsedEmail_ReturnsError()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void LoginUser_WithValidCredentials_ReturnsJWT()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void LoginUser_WithInvalidCredentials_ReturnsError()
+        {
+            Assert.Fail();
+        }
+    }
+}
