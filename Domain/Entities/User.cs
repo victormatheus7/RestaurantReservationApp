626581eb616e745a30f9a62f292774bae6666e16
Victor Castro <victormatheus.7@hotmail.com>
2023-10-22 23:37:10 -0300

    Changing the properties of the User class.

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,23 +1,43 @@
-﻿using Domain.Exceptions;
+﻿using Domain.Enum;
+using Domain.Exceptions;
 using Domain.Extensions;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 namespace Domain.Entities
 {
     public class User
     {
-        public string Email { get; }
-        public string Password { get; }
+        public string Email { get; private set; }
+        public byte[] PasswordHash { get; private set; }
+        public byte[] PasswordSalt { get; private set; }
+        public Role Role { get; private set; }
 
-        public User(string email, string password)
+        public User(string email, byte[] passwordHash, byte[] passwordSalt, Role role)
+        {
+            Email = email;
+            PasswordHash = passwordHash;
+            PasswordSalt = passwordSalt;
+            Role = role;
+        }
+
+        public static User CreateUser(string email, string password, Role role, byte[]? passwordSalt = null)
+        {
+            CheckUserData(email, password);
+
+            using var hmac = passwordSalt is not null ? new HMACSHA512(passwordSalt) : new HMACSHA512();
+
+            return new User(email,
+                hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
+                hmac.Key,
+                role);
+        }
+
+        private static void CheckUserData(string email, string password)
         {
             if (!IsValidEmail(email)) throw new InvalidEmailException(email);
 
             if (!IsValidPassword(password)) throw new InvalidPasswordException();
-
-
-            Email = email;
-            Password = password;
         }
 
         private static bool IsValidEmail(string email)
@@ -35,6 +55,5 @@ namespace Domain.Entities
         {
             return password.Length >= 6 && password.Length <= 12 && password.IsAllLettersOrDigits();
         }
-
     }
 }
--- UnitTests/Domain/UserTests.cs
+++ UnitTests/Domain/UserTests.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Enum;
 using Domain.Exceptions;
 
 namespace UnitTests.Domain
@@ -6,53 +7,58 @@ namespace UnitTests.Domain
     internal class UserTests
     {
         [Theory]
-        [TestCase("victor.castro@tests.com", "123456")]
-        [TestCase("victor.castro@tests.com.br", "123456abc")]
-        [TestCase("victor@castro.com", "abcdef123456")]
-        public void CreateUser_WithValidInformation_ReturnsCreatedUser(string email, string password)
+        [TestCase("victor.castro@tests.com", "123456", Role.Admin)]
+        [TestCase("victor.castro@tests.com.br", "123456abc", Role.Client)]
+        [TestCase("victor@castro.com", "abcdef123456", Role.Client)]
+        public void CreateUser_WithValidInformation_ReturnsCreatedUser(string email, string password, Role role)
         {
             // Arrange
             var user = new
             {
                 Email = email,
-                Password = password
+                Password = password,
+                Role = role
             };
 
             // Act
-            var userCreated = new User(email: user.Email, password: user.Password);
+            var userCreated1 = User.CreateUser(email: user.Email, password: user.Password, role: user.Role);
+            var userCreated2 = User.CreateUser(email: user.Email, password: user.Password, role: user.Role, passwordSalt: userCreated1.PasswordSalt);
 
             // Assert
             Assert.Multiple(() =>
             {
-                Assert.That(userCreated.Email, Is.EqualTo(user.Email));
-                Assert.That(userCreated.Password, Is.EqualTo(user.Password));
+                Assert.That(userCreated1.Email, Is.EqualTo(user.Email));
+                Assert.That(userCreated1.PasswordHash, Is.EqualTo(userCreated2.PasswordHash));
+                Assert.That(userCreated1.PasswordSalt, Is.EqualTo(userCreated2.PasswordSalt));
+                Assert.That(userCreated1.Role, Is.EqualTo(user.Role));
             });
         }
 
         [Theory]
-        [TestCase("", "123456")]
-        [TestCase("@victor.com", "123456")]
-        [TestCase("victor.castrotests.com.br", "123456abc")]
-        [TestCase("victor@castro", "abcdef123456")]
+        [TestCase("", "123456", Role.Admin)]
+        [TestCase("@victor.com", "123456", Role.Admin)]
+        [TestCase("victor.castrotests.com.br", "123456abc", Role.Admin)]
+        [TestCase("victor@castro", "abcdef123456", Role.Client)]
         [TestCase("very.long@domain.com" +
             "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz" +
             "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz" +
             "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz" +
             "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz" +
-            "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "123456")]
-        public void CreateUser_WithInvalidEmail_ThrowsInvalidEmailException(string email, string password)
+            "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "123456", Role.Client)]
+        public void CreateUser_WithInvalidEmail_ThrowsInvalidEmailException(string email, string password, Role role)
         {
             // Arrange
             var user = new
             {
                 Email = email,
-                Password = password
+                Password = password,
+                Role = role
             };
 
             try
             {
                 // Act
-                var userCreated = new User(email: user.Email, password: user.Password);
+                var userCreated = User.CreateUser(email: user.Email, password: user.Password, role: user.Role);
             }
             catch (Exception ex)
             {
@@ -66,11 +72,11 @@ namespace UnitTests.Domain
         }
 
         [Theory]
-        [TestCase("victor.castro@tests.com", "12345")]
-        [TestCase("victor.castro@tests.com", "123451234512345")]
-        [TestCase("victor.castro@tests.com.br", "123456@abc")]
-        [TestCase("victor@castro.com", "")]
-        public void CreateUser_WithInvalidPassword_ThrowsInvalidPasswordException(string email, string password)
+        [TestCase("victor.castro@tests.com", "12345", Role.Admin)]
+        [TestCase("victor.castro@tests.com", "123451234512345", Role.Admin)]
+        [TestCase("victor.castro@tests.com.br", "123456@abc", Role.Client)]
+        [TestCase("victor@castro.com", "", Role.Client)]
+        public void CreateUser_WithInvalidPassword_ThrowsInvalidPasswordException(string email, string password, Role role)
         {
             // Arrange
             var user = new
@@ -82,7 +88,7 @@ namespace UnitTests.Domain
             try
             {
                 // Act
-                var userCreated = new User(email: user.Email, password: user.Password);
+                var userCreated = User.CreateUser(email: user.Email, password: user.Password, role: role);
             }
             catch (Exception ex)
             {
